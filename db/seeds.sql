319245df2e499007f14d3ad3a4054c27b5d9b329
Adrit <axb5095@gmail.com>
2019-01-21 21:51:58 -0500

    my latest work

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Jake');
+INSERT INTO burger (name) VALUES ('Danny');
+INSERT INTO burger (name, eaten) VALUES ('Double burger', true);
+INSERT INTO burger (name, eaten) VALUES ('Cheese Burger', true);
+INSERT INTO burger (name, eaten) VALUES ('Beaconator', true);
+INSERT INTO burger (name) VALUES ('Owens');
